20700edc6684c8278bdc2647c308405916d31193
vansh210105 <vanshptl210@gmail.com>
2026-08-15 11:07:09 +0530

    pratice feature branch

--- setup/src/index.js
+++ setup/src/index.js
@@ -46,6 +46,7 @@ app.get("/user/:id/json",async(req,resp)=>{
 })
 
 app.post("user/:id/hash",(req,resp)=>{
+    console.log("testing feature branch")
     await redis.hset(`user:${req.params.id}:hash`,req.body);
     resp.json({savedAs:"hash"});
 })
